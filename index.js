ff59989b65796fceafbb447dad1788ed517a3fc4
FedYou <federicochurruarin0@gmail.com>
2024-11-17 12:47:04 -0300

    fix: :ambulance: Now the folder is created in the cache

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const id = require("./utils/id");
 module.exports = class YouTemp {
   constructor() {
     this.path = cachePath(`temp-${id()}`);
+    youfile.write.dir(this.path);
     process.on("exit", () => youfile.removeExists(this.path));
     jconsole.on("error", () => process.exit(0));
     process.on("SIGINT", () => process.exit(0));
