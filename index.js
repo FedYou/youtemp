1e2f4c60dbd8e226b6a8330c16b78afbfcaf599f
FedYou <federicochurruarin0@gmail.com>
2024-11-17 12:32:58 -0300

    feat: :heavy_plus_sign: Added functionality to YouTemp

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-module.exports = () => {};
+const youfile = require("youfile");
+const jconsole = require("@jumpcutking/console");
+const cachePath = require("./utils/cachePath");
+const id = require("./utils/id");
+
+module.exports = class YouTemp {
+  constructor() {
+    this.path = cachePath(`temp-${id()}`);
+    process.on("exit", () => youfile.removeExists(this.path));
+    jconsole.on("error", () => process.exit(0));
+    process.on("SIGINT", () => process.exit(0));
+  }
+  /**
+   * Clean the temporary folder
+   */
+  clear() {
+    youfile.removeExists(this.path);
+    youfile.write.dir(this.path);
+  }
+};
